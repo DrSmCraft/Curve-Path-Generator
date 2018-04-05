3fcc999c43a4e1408ef99c63bfd7239c696dd818
DrSmCraft <DrSmCraft@users.noreply.github.com>
2018-04-05 01:16:54 -0400

    Finishing Ui starting to work on logic and math

--- src/main.py
+++ src/main.py
@@ -19,24 +19,32 @@ import matplotlib.pyplot as plt
 
 import util
 
-starting_positions = {"Center":[90, 162], "Right":[90, 216], "Left":[90, 80]}
+starting_positions = {"Center": [90, 162], "Right": [90, 216], "Left": [90, 80]}
 
 # Functions
 def update(event):
     starting_radio_selection = util.get_radio_selection(starting_radio, starting_option_list)
     alliance_radio_selection = util.get_radio_selection(alliance_radio, alliance_option_list)
-    print("jjj")
-    #TODO Center image according to radio_selction
-    overlay.transpose([0, 0])
+
+
+    # Center image according to radio_selction
+    overlay.transpose(starting_positions[starting_radio_selection])
     
-    #TODO set  graph bounds to radio_selection
+    # set  graph bounds to radio_selection
     x_bounds = (0 - starting_positions[starting_radio_selection][0], overlay.dim()[0] - starting_positions[starting_radio_selection][0])
     y_bounds = (0 - starting_positions[starting_radio_selection][1], overlay.dim()[1] - starting_positions[starting_radio_selection][1])
     ax.set_xlim(x_bounds)
     ax.set_ylim(y_bounds)
 
+    # get vals from textboxes
+    try:
+        verts[0] = (util.get_number(startx), util.get_number(starty))
+        verts[1] = (util.get_number(endx), util.get_number(endy))
+    except:
+        verts.append((util.get_number(endx), util.get_number(endy)))
 
-
+    curve = util.Curve(ax, verts)
+    curve.draw()
 
 
 # Colors
@@ -47,12 +55,8 @@ BLACK = (0, 0, 0, 1)
 
 
 
-verts = [
-    (0, 0),
-    (100, 100),
-    (80, 200),
-    (8, 0)]
-
+verts = [(0, 0)]
+curves = []
 
 
 
@@ -74,10 +78,37 @@ ax_update_button = plt.axes([0.0, 0.0, 0.1, 0.1])
 text = "Update"
 update_button = widget.Button(ax_update_button, text)
 update_button.on_clicked(update)
-curve = util.Curve(ax, verts)
 overlay = util.Overlay(ax, "overlay.png")
 
-curve.draw()
+# Text Inputs
+startx_ax = plt.axes([.2, .1, .05, .05])
+starty_ax = plt.axes([.3, .1, .05, .05])
+endx_ax = plt.axes([.2, 0, .05, .05])
+endy_ax = plt.axes([.3, 0, .05, .05])
+mid1x_ax = plt.axes([.6, .1, .05, .05])
+mid1y_ax = plt.axes([.7, .1, .05, .05])
+mid2x_ax = plt.axes([.6, 0, .05, .05])
+mid2y_ax = plt.axes([.7, 0, .05, .05])
+
+startx = widget.TextBox(startx_ax, "Start X", initial="0")
+starty = widget.TextBox(starty_ax, "Start Y", initial="0")
+endx = widget.TextBox(endx_ax, "End X", initial="0")
+endy = widget.TextBox(endy_ax, "End Y", initial="0")
+mid1x = widget.TextBox(mid1x_ax, "Mid1 X", initial="0")
+mid1y = widget.TextBox(mid1y_ax, "Mid1 Y", initial="0")
+mid2x = widget.TextBox(mid2x_ax, "Mid2 X", initial="0")
+mid2y = widget.TextBox(mid2y_ax, "Mid2 Y", initial="0")
+
+
+# startx.on_submit(util.submit_number)
+# starty.on_submit(util.submit_number)
+# endx.on_submit(util.submit_number)
+# endy.on_submit(util.submit_number)
+# mid1x.on_submit(util.submit_number)
+# mid1y.on_submit(util.submit_number)
+# mid2x.on_submit(util.submit_number)
+# mid2y.on_submit(util.submit_number)
+
 overlay.draw()
 
 #Graph Bounds
--- src/util.py
+++ src/util.py
@@ -10,13 +10,16 @@ File for useful classes and functions.
 
 """
 
+print("Util loading")
 
 # Imports
 import matplotlib.path as path
 import matplotlib.patches as patches
 import matplotlib.image as m_image
 import PIL.Image as image
-
+import matplotlib.widgets as widget
+import numpy as np
+import scipy.misc as misc
 
 class Curve():
     def __init__(self, plot, verts):
@@ -28,8 +31,6 @@ class Curve():
         self.path = path.Path(self.verts, self.codes)
         self.patch = patches.PathPatch(self.path, facecolor="none", lw=2)
 
-
-
     def draw(self):
         self.plot.add_patch(self.patch)
 
@@ -38,17 +39,17 @@ class Overlay():
         self.plot = plot
         self.name = name
         self.im = image.open(self.name, "r")
+        self.img = m_image.imread(self.name)
+
 
     def draw(self):
-        self.img = m_image.imread(self.name)
         self.plot.imshow(self.img)
 
     def dim(self):
         return [self.im.width, self.im.height]
 
-    def transpose(self, transpose_coord):
-        #self.img.
-        self.plot.imshow(self.im, origin="upper")
+    def transpose(self, transposition_coord):
+        self.plot.imshow(self.img, extent=(0-transposition_coord[0], self.dim()[0] - transposition_coord[0], 0-transposition_coord[1], self.dim()[1] - transposition_coord[1]))
 
 class Line():
     def __init__(self, plot, start, end, color, thickness):
@@ -64,9 +65,64 @@ class Line():
         self.plot.add_patch(self.line)
 
 
+
+
 def get_radio_selection(radio, option_list):
     for select in range(len(radio.circles)):
         if radio.circles[select].get_facecolor()[0] < 0.5:
             return option_list[select]
 
+def submit_number(text):
+    try:
+        out = float(text)
+        return text
+    except:
+        return None
 
+
+def get_number(textbox):
+    try:
+        out = float(textbox.text)
+        return out
+    except:
+        return None
+
+
+# def bernstein_poly(i, n, t):
+#     """
+#      The Bernstein polynomial of n, i as a function of t
+#     """
+#
+#     return misc.comb(n, i) * ( t**(n-i) ) * (1 - t)**i
+#
+#
+# def bezier_curve(points, nTimes=1000):
+#     """
+#        Given a set of control points, return the
+#        bezier curve defined by the control points.
+#
+#        points should be a list of lists, or list of tuples
+#        such as [ [1,1],
+#                  [2,3],
+#                  [4,5], ..[Xn, Yn] ]
+#         nTimes is the number of time steps, defaults to 1000
+#
+#         See http://processingjs.nihongoresources.com/bezierinfo/
+#     """
+#
+#     nPoints = len(points)
+#     xPoints = np.array([p[0] for p in points])
+#     yPoints = np.array([p[1] for p in points])
+#
+#     t = np.linspace(0.0, 1.0, nTimes)
+#
+#     polynomial_array = np.array([ bernstein_poly(i, nPoints-1, t) for i in range(0, nPoints)   ])
+#
+#     xvals = np.dot(xPoints, polynomial_array)
+#     yvals = np.dot(yPoints, polynomial_array)
+#
+#     return xvals, yvals
+
+#print(bezier_curve([(0, 0), (100, 0), (50, 50)]))
+#print(type(print(quadBrezPoints((0, 0), (100, 0), (50, 50), 5))))
+print("Util loaded")
