cab6a5ff3f87e2e45cd301bbc0c143e0531ea3cd
DrSmCraft <DrSmCraft@users.noreply.github.com>
2018-04-09 09:04:20 -0400

    Added resource_path method
    Added resource_path method to fix "overlay image not found" error.

--- src/util.py
+++ src/util.py
@@ -150,3 +150,15 @@ def put_text(textbox, text):
 def write(location, text):
     with open(location, 'a+') as file:
         file.write(text + '\n\n')
+        
+        
+ # https://stackoverflow.com/questions/31836104/pyinstaller-and-onefile-how-to-include-an-image-in-the-exe-file
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
